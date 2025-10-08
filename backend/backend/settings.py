0fc7ef2e6ef92d343bbf2cc819c41066827d3001
Ellianna Principe <elliannaprincipe06@gmail.com>
2025-10-09 05:35:00 +0800

    Use basic StaticFilesStorage

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -231,7 +231,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage", 
     },
 }
 
