a4bc819ec71023b2ac0682c121c34bbbbc996a92
Ellianna Principe <elliannaprincipe06@gmail.com>
2025-10-09 05:31:04 +0800

    Fix build.sh order and add clear flag

--- backend/build.sh
+++ backend/build.sh
@@ -4,8 +4,8 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
+python manage.py collectstatic --no-input --clear
+
 echo "Running migrations..."
 python manage.py migrate --noinput
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
