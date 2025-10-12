d116c5e3da22fb7d7de3d341b6aa5e5ebd57ec0d
irah <irahrapunzelplumbera@iskolarngbayan.pup.edu.ph>
2025-10-12 21:28:43 +0800

    minor fix

--- components/trade-cards/explore-card.tsx
+++ components/trade-cards/explore-card.tsx
@@ -234,16 +234,24 @@ export default function ExploreCard({
 
         {/* Needs + Offer */}
         <div className="flex justify-between items-start gap-4 flex-wrap">
+          {/* Needs */}
           <div className="flex flex-col gap-2 flex-1 min-w-[45%] items-start">
             <span className="text-sm text-white/80 font-medium">Needs</span>
-            <div className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-sm text-white/90 w-fit">
+            <div
+              className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-sm text-white/90 w-full max-w-full truncate"
+              title={need} // para makita full text on hover
+            >
               {need}
             </div>
           </div>
 
+          {/* Can offer */}
           <div className="flex flex-col gap-2 flex-1 min-w-[45%] items-end">
             <span className="text-sm text-white/80 font-medium">Can offer</span>
-            <div className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-sm text-white/90 w-fit text-right">
+            <div
+              className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-sm text-white/90 w-full max-w-full truncate text-right"
+              title={offer}
+            >
               {offer}
             </div>
           </div>
