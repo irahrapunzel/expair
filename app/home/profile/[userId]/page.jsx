92b829d569e9871a6ac7de51cf195bdd3bdd134b
irah <irahrapunzelplumbera@iskolarngbayan.pup.edu.ph>
2025-10-14 07:06:04 +0800

    minor fix

--- app/home/profile/[userId]/page.jsx
+++ app/home/profile/[userId]/page.jsx
@@ -3699,12 +3699,12 @@ export default function ProfilePage() {
                           </div>
 
                           {/* Needs + Offer */}
-                          <div className="flex justify-between items-start gap-4 flex-wrap mb-3">
+                          <div className="flex justify-between items-start gap-4 mb-3 w-full">
                             {/* Needs */}
-                            <div className="flex flex-col gap-2 flex-1 items-start">
+                            <div className="flex flex-col gap-2 w-1/2">
                               <span className="text-sm text-white/80 font-medium">Needs</span>
                               <div
-                                className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-sm text-white/90 max-w-[160px] sm:max-w-[180px] md:max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                                className="px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-sm text-white/90 w-full overflow-hidden text-ellipsis whitespace-nowrap"
                                 title={trade.reqname}
                               >
                                 {trade.reqname}
@@ -3712,10 +3712,10 @@ export default function ProfilePage() {
                             </div>
 
                             {/* Can offer */}
-                            <div className="flex flex-col gap-2 flex-1 items-end">
+                            <div className="flex flex-col gap-2 w-1/2 items-end">
                               <span className="text-sm text-white/80 font-medium">Can offer</span>
                               <div
-                                className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-sm text-white/90 max-w-[160px] sm:max-w-[180px] md:max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap text-right"
+                                className="px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-sm text-white/90 w-full overflow-hidden text-ellipsis whitespace-nowrap text-right"
                                 title={trade.offer || trade.matching_skill}
                               >
                                 {trade.offer || trade.matching_skill}
