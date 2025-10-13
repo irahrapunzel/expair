ae57ec0ec98373c88a8d00eded0442c406ca8779
irah <irahrapunzelplumbera@iskolarngbayan.pup.edu.ph>
2025-10-14 07:55:37 +0800

    minor fix

--- components/trade-cards/trade-request-info.jsx
+++ components/trade-cards/trade-request-info.jsx
@@ -16,7 +16,7 @@ export default function TradeRequestInfo({ requested, exchange }) {
         </div>
 
         {/* In exchange, you'll provide */}
-        <div className="flex flex-col gap-2 flex-1 min-w-60%] items-end">
+        <div className="flex flex-col gap-2 flex-1 min-w-80%] items-end">
           <span className="text-[18px] text-white/80 font-medium">In exchange, you'll provide</span>
           <div
             className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#0038FF] bg-[#0038FF33] text-[15px] text-white/90 max-w-[180px] overflow-hidden text-ellipsis whitespace-nowrap text-right"
