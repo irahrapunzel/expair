08ac5b21ae8ee2be34a482a98c3e0aa398722fd7
irah <irahrapunzelplumbera@iskolarngbayan.pup.edu.ph>
2025-10-14 07:36:51 +0800

    minor fix

--- components/trade-cards/trade-request-info.jsx
+++ components/trade-cards/trade-request-info.jsx
@@ -8,7 +8,7 @@ export default function TradeRequestInfo({ requested, exchange }) {
         <div className="flex flex-col flex-1 min-w-[200px]">
           <span className="text-[18px] text-white mb-2">You're requesting</span>
           <div
-            className="inline-block px-[15px] py-[8px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-[15px] text-white max-w-full sm:max-w-[300px] truncate cursor-default transition-all hover:scale-[1.02]"
+            className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-[15px] text-white max-w-full sm:max-w-[300px] truncate cursor-default transition-all hover:scale-[1.02]"
             title={requested}
           >
             {requested}
@@ -21,7 +21,7 @@ export default function TradeRequestInfo({ requested, exchange }) {
             In exchange, you'll provide
           </span>
           <div
-            className="inline-block px-[15px] py-[8px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-[15px] text-white max-w-full sm:max-w-[300px] truncate cursor-default transition-all hover:scale-[1.02] text-right"
+            className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-[15px] text-white max-w-full sm:max-w-[300px] truncate cursor-default transition-all hover:scale-[1.02] text-right"
             title={exchange}
           >
             {exchange}
