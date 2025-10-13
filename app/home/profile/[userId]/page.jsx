4575c1c31904f412a8c76161004231247aaa183b
irah <irahrapunzelplumbera@iskolarngbayan.pup.edu.ph>
2025-10-14 06:35:41 +0800

    minor fix

--- app/home/profile/[userId]/page.jsx
+++ app/home/profile/[userId]/page.jsx
@@ -3707,19 +3707,24 @@ export default function ProfilePage() {
 
                           {/* Needs + Offer */}
                           <div className="flex justify-between items-start gap-4 flex-wrap mb-3">
+                            {/* Needs */}
                             <div className="flex flex-col gap-2 flex-1 min-w-[45%] items-start">
-                              <span className="text-sm text-white/80 font-medium">
-                                Needs
-                              </span>
-                              <div className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-sm text-white/90 w-fit">
+                              <span className="text-sm text-white/80 font-medium">Needs</span>
+                              <div
+                                className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#5A5AFF] bg-[#5A5AFF33] text-sm text-white/90 max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap"
+                                title={trade.reqname}
+                              >
                                 {trade.reqname}
                               </div>
                             </div>
+
+                            {/* Can offer */}
                             <div className="flex flex-col gap-2 flex-1 min-w-[45%] items-end">
-                              <span className="text-sm text-white/80 font-medium">
-                                Can offer
-                              </span>
-                              <div className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-sm text-white/90 w-fit text-right">
+                              <span className="text-sm text-white/80 font-medium">Can offer</span>
+                              <div
+                                className="inline-block px-[15px] py-[7px] rounded-[15px] border-[2px] border-[#906EFF] bg-[#906EFF33] text-sm text-white/90 max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap text-right"
+                                title={trade.offer || trade.matching_skill}
+                              >
                                 {trade.offer || trade.matching_skill}
                               </div>
                             </div>
